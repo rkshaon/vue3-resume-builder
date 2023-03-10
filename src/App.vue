e5adfdb35e3d139ced9e53c04210d650baabcecc
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-03-10 23:30:51 +0600

    Home Page - setup

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Home></Home>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Home from './components/HomeComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Home
+}
 }
 </script>
 
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <p>Theme 1</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeComponent',
+    props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
