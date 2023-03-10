306ce27a2cb274a56b4a95a3a422cdc7700f5d23
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-03-11 00:11:21 +0600

    bootstrap missing fixed

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
 createApp(App).use(router).mount('#app')
